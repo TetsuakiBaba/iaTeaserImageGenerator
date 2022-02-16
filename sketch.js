5fa1b212d8707a707a41a4ef19d250d3f3bc3b04
Tetsuaki Baba <baba@bagpipe.local>
2022-02-16 12:18:20 +0900

    途中だけど急ぎ

--- sketch.js
+++ sketch.js
@@ -33,8 +33,9 @@ function preload() {
 
 function setup() {
     console.log(navigator.userAgent);
+
     // graphics stuff:
-    let client_w = document.getElementById('container').clientWidth;
+    let client_w = document.getElementById('sketch-holder').clientWidth;
     var canvas = createCanvas(client_w, (client_w) * (9.0 / 16.0));
 
 
@@ -47,11 +48,36 @@ function setup() {
     canvas.parent('sketch-holder');
     frameRate(15);
 
+
+
+
+
+
+    // apply GET URL Prames to graphics
+    const urlParams = new URLSearchParams(window.location.search);
+    var url_gfont = 'undefined';
+    if (urlParams.get('param_set') == 'true') {
+        console.log("URL Parameters mode");
+        const url_title = urlParams.get('title');
+        const url_abstract = urlParams.get('abstract');
+        const url_geometry = urlParams.get('geometry');
+        const url_color_scheme = urlParams.get('color_scheme');
+        const url_curved = urlParams.get('curved');
+        url_gfont = urlParams.get('gfont');
+        document.querySelector('#input_name').value = url_title;
+        document.querySelector('#input_affiliation').value = url_abstract;
+        document.querySelector('#number_geometry').value = url_geometry;
+    }
+    else {
+        url_gfont = 'undefined';
+    }
+
+
     geometry = new adadaGeometry(5, image_logo,
         document.querySelector("#input_name").value,
         document.querySelector("#input_affiliation").value
     );
-
+    geometry.setColorScheme(document.getElementById("select_color_scheme").value);
     select('#button_repattern').mouseClicked(Repattern);
     select('#input_name').input(changedName);
     select('#input_affiliation').input(changedAffiliation);
@@ -59,9 +85,7 @@ function setup() {
     select('#number_geometry').changed(changedGeometry);
     select('#select_color_scheme').changed(changedColorScheme);
     select('#button_curved').mouseClicked(setCurved);
-
-    geometry.setColorScheme(document.getElementById("select_color_scheme").value);
-
+    //////////////////////////////////
 
     selectGfont({
         key: 'AIzaSyAQgrjj1umZWGOdEUFU5jz_cWYdWpvPc3A',
@@ -70,15 +94,20 @@ function setup() {
         sort: 'popularity',
         onSelectFonte: 'sGFselecionado'
     }).then(function () {
-        var obj = document.getElementById('selectGFont');
-        var idx = obj.selectedIndex;
-        g_font = obj.options[idx].text;
-        g_font = g_font.replace('serif', '');
-        console.log(g_font)
-        geometry.setFont(g_font);
+        console.log(url_gfont);
+        if (url_gfont == 'undefined') {
+            var obj = document.getElementById('selectGFont');
+            var idx = obj.selectedIndex;
+            g_font = obj.options[idx].text;
+            g_font = g_font.replace('serif', '');
+            console.log(g_font)
+            geometry.setFont(g_font);
+        } else {
+            geometry.setFont(url_gfont);
+        }
+
     }).catch(function (erro) {
         console.error(erro);
-
     });
 
 }
@@ -125,7 +154,7 @@ function download() {
 }
 
 function windowResized() {
-    let client_w = document.getElementById('container').clientWidth;
+    let client_w = document.getElementById('sketch-holder').clientWidth;
     resizeCanvas(client_w, (client_w) * 9 / 16);
 
 }
